3759bb92c4632792ad009f175b8c3271ce145a13
Sean McDermott <mcderm.sean@gmail.com>
2017-04-10 11:22:25 -0700

    Links to Homepage and Movies page work

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET movies listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('movies', {title: 'Movies!'});
 });
 
 module.exports = router;
