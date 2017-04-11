c615df66d624f7104d4b4c8bb9bfe8021387c5ea
Sean McDermott <mcderm.sean@gmail.com>
2017-04-10 21:48:23 -0700

    added SHOW method and some styling for movieIndex

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/knex.js');
 
 /* GET movies listing. */
-router.get('/', function(req, res, next) {
-  res.render('movies', {title: 'Movies!'});
+router.get('/', (req, res, next) => {
+  db('movies')
+  .then(moviesIndex => {
+    res.render('movies', {title: 'Movies Index Page!', moviesIndex});
+    knex.destroy();
+  }).catch(err => {
+    console.error(err);
+    knex.destroy();
+    process.exit(1);
+  });
 });
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  db('movies')
+  .where('id', id)
+  .then(moviesIndex => {
+    res.render('movies', {title: 'Here\'s the movie you wanted!', moviesIndex});
+    knex.destroy();
+  }).catch(err => {
+    console.error(err);
+    knex.destroy();
+    process.exit(1);
+  });
+})
 
 module.exports = router;
